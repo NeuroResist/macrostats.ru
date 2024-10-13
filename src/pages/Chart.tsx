b8902a5dac7638e59c5a45088f8ba13429b8cc79
NeuroResist <zeit7@rambler.ru>
2024-10-13 23:36:13 +0300

    feat: Исправления

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -1,4 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format, parse } from "date-fns";
 import {
   CartesianGrid,
   Legend,
@@ -9,11 +13,21 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import DatePicker from "react-datepicker";
-import { format, parse } from "date-fns";
 import { data } from "../constants.tsx"; // Предполагаю, что данные загружаются из constants.tsx
 
 export const Chart = () => {
+  const location = useLocation();
+
+  const {
+    full_name,
+    second_full_units,
+    seasonality,
+    period,
+    last_observation,
+    updated,
+    // eslint-disable-next-line no-unsafe-optional-chaining
+  } = location.state?.item;
+
   const [startDate, setStartDate] = useState<Date | undefined>(undefined);
   const [endDate, setEndDate] = useState<Date | undefined>(undefined);
   const [filteredData, setFilteredData] = useState(data); // Состояние для отфильтрованных данных
@@ -32,6 +46,12 @@ export const Chart = () => {
     return Math.round(value / 1e6); // Обрезаем до миллионов
   };
 
+  // Модифицируем данные для графика (округляем значения government_revenue)
+  const modifiedData = filteredData.map((item) => ({
+    ...item,
+    government_revenue: trimGovernmentRevenue(item.government_revenue),
+  }));
+
   // Фильтрация данных по диапазону дат
   useEffect(() => {
     if (startDate && endDate) {
@@ -45,14 +65,74 @@ export const Chart = () => {
     }
   }, [startDate, endDate]);
 
-  // Модифицируем данные для графика (округляем значения government_revenue)
-  const modifiedData = filteredData.map((item) => ({
-    ...item,
-    government_revenue: trimGovernmentRevenue(item.government_revenue),
-  }));
-
   return (
     <div className="px-20 mx-auto max-w-[1200px]">
+      <section className="text-headerSection text-Bright-blue mb-14 bg-Light-gray h-[50px] flex items-center px-14 justify-between">
+        <h2>{full_name}</h2>
+
+        <div className="bg-Bright-blue text-white text-sm h-20 w-100 flex items-center justify-center">
+          .xlsx
+        </div>
+      </section>
+
+      <div className="px-10 flex mb-40">
+        <div className="flex bg-amber-100 w-[500px] text-sm justify-between mr-[170px]">
+          <ul className="flex flex-col">
+            <li>last_observation:</li>
+            <li className="mb-2">{last_observation}</li>
+            <li>updated: {updated}</li>
+          </ul>
+
+          <ul className="flex flex-col w-[180px]">
+            <li>units:</li>
+            <li>{second_full_units}</li>
+            <li>{seasonality}</li>
+          </ul>
+
+          <ul className="flex flex-col w-72">
+            <li>period:</li>
+            <li>{period}</li>
+          </ul>
+        </div>
+
+        <div className="flex justify-between w-300">
+          <div className="flex items-center mr-14">
+            <label className="mr-10 inline-block">from:</label>
+
+            <DatePicker
+              className="bg-Light-gray h-36 max-w-100"
+              selected={startDate}
+              onChange={(date) => {
+                setStartDate(date || undefined);
+                if (endDate && date && date > endDate) {
+                  setEndDate(undefined); // Сбросить конечную дату, если начальная больше конечной
+                }
+              }}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              dateFormat="dd.MM.yyyy"
+            />
+          </div>
+
+          <div className="flex items-center">
+            <label className="mr-10 inline-block">to:</label>
+
+            <DatePicker
+              className="bg-Light-gray h-36 flex max-w-100"
+              selected={endDate}
+              onChange={(date) => setEndDate(date || undefined)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              dateFormat="dd.MM.yyyy"
+              disabled={!startDate}
+            />
+          </div>
+        </div>
+      </div>
+
       <div>
         {formatDate(startDate)} — {formatDate(endDate)}
       </div>
